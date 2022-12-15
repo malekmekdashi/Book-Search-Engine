25b4cc5a18dc8d8e3ddbc28e95481be42ad6e6a9
malek mekdashi <malekmekdashi@gmail.com>
2022-12-15 00:42:59 -0800

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.PORT || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
